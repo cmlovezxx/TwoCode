fed94e90eb441c7ae72172f8b9b84bef275347aa
ChengRui <chengruihh@gmail.com>
2016-02-29 22:16:43 +0800

    文档目录界面ok

--- src/com/zx/twocode/adapter/DocListviewAdapter.java
+++ src/com/zx/twocode/adapter/DocListviewAdapter.java
@@ -1,66 +1,124 @@
 package com.zx.twocode.adapter;
 
 import android.content.Context;
+import android.database.DataSetObserver;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.zx.twocode.R;
 import com.zx.twocode.bean.DocumentListBean;
 
-public class DocListviewAdapter extends BaseAdapter {
+public class DocListviewAdapter extends BaseExpandableListAdapter {
 
 	private Context context;
 	private DocumentListBean docList;
-	
+
 	public DocListviewAdapter(Context context, DocumentListBean doc) {
 		super();
 		this.context = context;
 		this.docList = doc;
 	}
 
+	private final class GroupViewHolder {
+		TextView label;
+	}
+
+	private final class ChildViewHolder {
+		TextView label;
+	}
+
 	@Override
-	public int getCount() {
+	public int getGroupCount() {
 		return docList.getDocumentList().size();
 	}
 
 	@Override
-	public Object getItem(int position) {
-		return docList.getDocumentList().get(position);
+	public int getChildrenCount(int groupPosition) {
+		return docList.getDocumentList().get(groupPosition).getFolder().size();
 	}
 
 	@Override
-	public long getItemId(int position) {
-		return position;
+	public Object getGroup(int groupPosition) {
+		return docList.getDocumentList().get(groupPosition);
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder viewHolder;
-		Log.e("position", position+","+this.getCount());
-		if(convertView == null){
-			convertView = View.inflate(context, R.layout.doc_list_item, null);
-			viewHolder = new ViewHolder();
-			viewHolder.icon = (ImageView) convertView.findViewById(R.id.doc_list_icon);
-			viewHolder.label = (TextView) convertView.findViewById(R.id.doc_list_label);
-			viewHolder.arrow = (ImageView) convertView.findViewById(R.id.doc_list_arrow);
-			convertView.setTag(viewHolder);
-		}else{
-			viewHolder = (ViewHolder) convertView.getTag();
+	public Object getChild(int groupPosition, int childPosition) {
+		return docList.getDocumentList().get(groupPosition).getFolder()
+				.get(childPosition);
+	}
+
+	@Override
+	public long getGroupId(int groupPosition) {
+		return groupPosition * 100;
+	}
+
+	@Override
+	public long getChildId(int groupPosition, int childPosition) {
+		return groupPosition * 100 + childPosition;
+
+	}
+
+	@Override
+	public View getGroupView(int groupPosition, boolean isExpanded,
+			View convertView, ViewGroup parent) {
+		GroupViewHolder groupViewHolder;
+		if (convertView == null) {
+			convertView = View.inflate(context, R.layout.doc_list_folder, null);
+			groupViewHolder = new GroupViewHolder();
+
+			groupViewHolder.label = (TextView) convertView
+					.findViewById(R.id.doc_list_label);
+
+			convertView.setTag(groupViewHolder);
+		} else {
+			groupViewHolder = (GroupViewHolder) convertView.getTag();
 		}
-		
-		viewHolder.label.setText(docList.getDocumentList().get(position).getFolderName());
-		
+
+		groupViewHolder.label.setText(docList.getDocumentList()
+				.get(groupPosition).getFolderName());
+
 		return convertView;
 	}
-	private final class ViewHolder
-	{
-		ImageView icon;
-		TextView label;
-		ImageView arrow;
+
+	@Override
+	public View getChildView(int groupPosition, int childPosition,
+			boolean isLastChild, View convertView, ViewGroup parent) {
+		ChildViewHolder childViewHolder;
+		if (convertView == null) {
+			convertView = View.inflate(context, R.layout.doc_list_file, null);
+			childViewHolder = new ChildViewHolder();
+			childViewHolder.label = (TextView) convertView
+					.findViewById(R.id.doc_list_filename);
+			convertView.setTag(childViewHolder);
+		} else {
+			childViewHolder = (ChildViewHolder) convertView.getTag();
+		}
+
+		childViewHolder.label.setText(docList.getDocumentList()
+				.get(groupPosition).getFolder().get(childPosition)
+				.getDocumentName());
+
+		return convertView;
+	}
+
+	
+	@Override
+	public boolean hasStableIds() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 }
--- src/com/zx/twocode/fragment/impl/DocumentFragment.java
+++ src/com/zx/twocode/fragment/impl/DocumentFragment.java
@@ -1,10 +1,13 @@
 package com.zx.twocode.fragment.impl;
 
 import android.os.SystemClock;
-import android.util.Log;
 import android.view.View;
-import android.widget.ListView;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.zx.twocode.R;
 import com.zx.twocode.adapter.DocListviewAdapter;
@@ -14,22 +17,25 @@ import com.zx.twocode.fragment.BaseFragment;
 public class DocumentFragment extends BaseFragment {
 	private TextView equipmentName;
 	private TextView currentDirectory;
-	private ListView docList;
+	// private ListView docList;
+	private ExpandableListView docListView;
 
 	@Override
 	public View createView() {
-//		TextView view = new TextView(getActivity());
 		View view = View.inflate(context, R.layout.fragment_document, null);
-		this.equipmentName = (TextView) view.findViewById(R.id.zx_tv_equipmentname);
-		this.currentDirectory = (TextView) view.findViewById(R.id.zx_tv_currentdirectory);
-		this.docList = (ListView) view.findViewById(R.id.zx_lv_documentlist);
-//		view.setText("第三个页面");
+		this.equipmentName = (TextView) view
+				.findViewById(R.id.zx_tv_equipmentname);
+		this.currentDirectory = (TextView) view
+				.findViewById(R.id.zx_tv_currentdirectory);
+		// this.docList = (ListView) view.findViewById(R.id.zx_lv_documentlist);
+		this.docListView = (ExpandableListView) view
+				.findViewById(R.id.zx_lv_documentlist);
 		return view;
 	}
 
 	@Override
 	public void refreshView() {
-		
+
 		new MyHttpTask<DocumentListBean>() {
 
 			@Override
@@ -39,31 +45,62 @@ public class DocumentFragment extends BaseFragment {
 				// + params[0] + "=" + params[1]);
 				// String json = httpResult.getString();
 				SystemClock.sleep(1000);
-//				DocumentProtocal docProtocal = new DocumentProtocal();
-//				DocumentListBean docListBean = docProtocal.load(params);
-				
-				//--------for test--------------
+				// DocumentProtocal docProtocal = new DocumentProtocal();
+				// DocumentListBean docListBean = docProtocal.load(params);
+
+				// --------for test--------------
 				DocumentListBean docListBean = new DocumentListBean();
 				docListBean.setTestData();
-				//------------------------------
+				// ------------------------------
 				return docListBean;
 			}
 
 			@Override
-			protected void setViewInfo(DocumentListBean result) {
+			protected void setViewInfo(final DocumentListBean result) {
 				// TODO Auto-generated method stub
 				equipmentName.setText(result.getEquipmentname());
-				DocListviewAdapter docAdapter = new DocListviewAdapter(context, result);
-				docList.setAdapter(docAdapter);
-				Log.e("setviewinfo", "0");
+				DocListviewAdapter docAdapter = new DocListviewAdapter(context,
+						result);
+				docListView.setGroupIndicator(null);
+				docListView.setAdapter(docAdapter);
+				docListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+					
+					@Override
+					public void onGroupExpand(int groupPosition) {
+						currentDirectory.setText(result.getDocumentList().get(groupPosition).getFolderName());
+					}
+				});
+				docListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+					
+					@Override
+					public void onGroupCollapse(int groupPosition) {
+						currentDirectory.setText("");
+					}
+				});
+				docListView.setOnChildClickListener(new OnChildClickListener() {
+
+					@Override
+					public boolean onChildClick(ExpandableListView parent,
+							View v, int groupPosition, int childPosition,
+							long id) {
+						
+						currentDirectory.setText(result.getDocumentList().get(groupPosition).getFolderName());
+						Toast.makeText(context,
+								result.getDocumentList().get(groupPosition)
+										.getFolder().get(childPosition)
+										.getDocumentUrl(), Toast.LENGTH_SHORT).show();
+						return true;
+					}
+				});
 			}
 
 		}.executeProxy();
 	};
+
 	@Override
 	protected String[] getParams() {
 		// TODO Auto-generated method stub
-		String[] strings = {"requestcode","006"};
+		String[] strings = { "requestcode", "006" };
 		return strings;
 	}
 }
